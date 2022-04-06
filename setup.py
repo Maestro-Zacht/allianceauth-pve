85a918094aa2d204aef03df57a45eb233ec35b17
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-04-06 22:41:47 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Matteo Ghia",
@@ -20,25 +20,34 @@ setup(
     python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Operating System :: POSIX :: Linux',
         'Intended Audience :: Developers',
+        'Framework :: Django',
+        'Framework :: Django :: 3.2',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     description="PvE tool for AllianceAuth",
     install_requires=requirements,
     license="GNU General Public License v3",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='allianceauth_pve',
     name='allianceauth_pve',
     packages=find_packages(include=['allianceauth_pve', 'allianceauth_pve.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/Maestro-Zacht/allianceauth_pve',
+    url='https://github.com/Maestro-Zacht/allianceauth-pve',
     version='0.1.0',
     zip_safe=False,
 )
