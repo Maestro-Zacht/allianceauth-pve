ef109d7c8fe52cae81253ca1d5808a570eead263
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-04-07 12:41:16 +0200

    added aa requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ with open('README.md') as readme_file:
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = []
+requirements = [
+    'allianceauth>=2.11.2,<3.0.0',
+]
 
 test_requirements = []
 
