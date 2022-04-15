8f47f1cc5121d64560f9bdcb95298d34d888b42c
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-04-15 18:30:01 +0200

    registered menu hook

--- allianceauth_pve/auth_hooks.py
+++ allianceauth_pve/auth_hooks.py
@@ -1,7 +1,17 @@
 from allianceauth import hooks
-from allianceauth.services.hooks import UrlHook
+from allianceauth.services.hooks import UrlHook, MenuItemHook
 
 from . import urls
+
+
+class PveMenuItemHook(MenuItemHook):
+    def __init__(self):
+        super().__init__("PvE Tool", "fas fa-wallet", "allianceauth_pve:index", navactive=['allianceauth_pve:'])
+
+
+@hooks.register('menu_item_hook')
+def register_menu():
+    return PveMenuItemHook()
 
 
 @hooks.register('url_hook')
