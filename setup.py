5aea3159827aa136124b498ae91ea7a13ca4352d
Matteo Ghia <matteo.ghia.2001@gmail.com>
2022-05-09 14:47:24 +0200

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Maestro-Zacht/allianceauth-pve',
-    version='1.1.0',
+    version='1.1.1',
     zip_safe=False,
 )
