97129a25482d60bfa126e793258001640a6d0c82
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-01 11:55:12 +0200

    removed failing test

--- allianceauth_pve/tests/test_url.py
+++ allianceauth_pve/tests/test_url.py
@@ -3,8 +3,5 @@ from django.test import SimpleTestCase
 
 class TestUrls(SimpleTestCase):
 
-    def test_test(self):
-        self.assertTrue(False)
-
     def test_test_bis(self):
         self.assertTrue(True)
