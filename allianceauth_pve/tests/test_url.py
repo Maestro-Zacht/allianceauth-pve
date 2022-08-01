52735f74b7e1544aace72b941c2b8f450f1626bc
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-01 11:49:20 +0200

    test github config

---
+++ allianceauth_pve/tests/test_url.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_test(self):
+        self.assertTrue(False)
+
+    def test_test_bis(self):
+        self.assertTrue(True)
