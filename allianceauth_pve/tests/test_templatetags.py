bf76d3d8c1bab4f4ec98d5b5cb7a89d12c8e195b
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-05 10:10:42 +0200

    added test for versioned static

---
+++ allianceauth_pve/tests/test_templatetags.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.template import Context, Template
+
+from allianceauth_pve import __version__
+
+
+class TestPvEVersionedStatic(TestCase):
+    """
+    Tests for allianceauth_pve_versioned_static template tag
+    """
+
+    def test_versioned_static(self):
+        """
+        Test should return static URL string with version
+        :return:
+        """
+
+        context = Context({"version": __version__})
+        template_to_render = Template(
+            "{% load allianceauth_pve_versioned_static %}"
+            "{% allianceauth_pve_static 'allianceauth_pve/css/custom_checkbox.css' %}"
+        )
+
+        rendered_template = template_to_render.render(context)
+
+        self.assertInHTML(
+            f'/static/allianceauth_pve/css/custom_checkbox.css?v={context["version"]}',
+            rendered_template,
+        )
