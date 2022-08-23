74844e33683aaf9d64755e4733328c4d36b3f8b0
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-23 14:46:06 +0200

    added new role formset tests

---
+++ allianceauth_pve/tests/test_forms.py
@@ -0,0 +1,50 @@
+from django.test import SimpleTestCase
+
+from ..forms import NewRoleFormSet
+
+
+class TestNewRoleFormSet(SimpleTestCase):
+
+    def test_valid(self):
+        valid_data = {
+            'roles-TOTAL_FORMS': ['5'],
+            'roles-INITIAL_FORMS': ['1'],
+            'roles-MIN_NUM_FORMS': ['1'],
+            'roles-MAX_NUM_FORMS': ['1000'],
+            'roles-0-name': ['roleS0R0'],
+            'roles-0-value': ['1'],
+            'roles-1-name': ['roleS0R1'],
+            'roles-1-value': ['1'],
+            'roles-2-name': ['roleS0R2'],
+            'roles-2-value': ['2'],
+            'roles-3-name': ['roleS0R3'],
+            'roles-3-value': ['3'],
+            'roles-4-name': ['roleS0R4'],
+            'roles-4-value': ['4']
+        }
+
+        role_form = NewRoleFormSet(valid_data, prefix='roles')
+
+        self.assertTrue(role_form.is_valid())
+
+    def test_invalid(self):
+        invalid_data = {
+            'roles-TOTAL_FORMS': ['5'],
+            'roles-INITIAL_FORMS': ['1'],
+            'roles-MIN_NUM_FORMS': ['1'],
+            'roles-MAX_NUM_FORMS': ['1000'],
+            'roles-0-name': ['roleS0R1'],
+            'roles-0-value': ['1'],
+            'roles-1-name': ['roleS0R1'],
+            'roles-1-value': ['1'],
+            'roles-2-name': ['roleS0R2'],
+            'roles-2-value': ['2'],
+            'roles-3-name': ['roleS0R3'],
+            'roles-3-value': ['3'],
+            'roles-4-name': ['roleS0R4'],
+            'roles-4-value': ['4']
+        }
+
+        role_form = NewRoleFormSet(invalid_data)
+
+        self.assertFalse(role_form.is_valid())
