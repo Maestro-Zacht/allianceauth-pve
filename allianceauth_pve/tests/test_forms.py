21297121be4e2553376366056958e90fbb55c36a
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-23 17:06:18 +0200

    added test for new role error

--- allianceauth_pve/tests/test_forms.py
+++ allianceauth_pve/tests/test_forms.py
@@ -30,7 +30,7 @@ class TestNewRoleFormSet(SimpleTestCase):
 
         self.assertTrue(role_form.is_valid())
 
-    def test_invalid(self):
+    def test_duplicated_invalid(self):
         invalid_data = {
             'roles-TOTAL_FORMS': '5',
             'roles-INITIAL_FORMS': '0',
@@ -38,6 +38,28 @@ class TestNewRoleFormSet(SimpleTestCase):
             'roles-MAX_NUM_FORMS': '1000',
             'roles-0-name': 'roleS0R1',
             'roles-0-value': '1',
+            'roles-1-name': 'roleS0R1',
+            'roles-1-value': '1',
+            'roles-2-name': 'roleS0R2',
+            'roles-2-value': '2',
+            'roles-3-name': 'roleS0R3',
+            'roles-3-value': '3',
+            'roles-4-name': 'roleS0R4',
+            'roles-4-value': '4'
+        }
+
+        role_form = NewRoleFormSet(invalid_data, prefix='roles')
+
+        self.assertFalse(role_form.is_valid())
+
+    def test_error_invalid(self):
+        invalid_data = {
+            'roles-TOTAL_FORMS': '5',
+            'roles-INITIAL_FORMS': '0',
+            'roles-MIN_NUM_FORMS': '1',
+            'roles-MAX_NUM_FORMS': '1000',
+            'roles-0-name': 'roleS0R0',
+            'roles-0-value': '0',
             'roles-1-name': 'roleS0R1',
             'roles-1-value': '1',
             'roles-2-name': 'roleS0R2',
