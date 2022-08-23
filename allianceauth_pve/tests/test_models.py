b76f5aeb96730a17c3711e588714dcad59173a26
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-23 18:04:35 +0200

    added rotation tests

---
+++ allianceauth_pve/tests/test_models.py
@@ -0,0 +1,94 @@
+from django.test import TestCase
+from django.utils import timezone
+
+from allianceauth.tests.auth_utils import AuthUtils
+
+from ..models import Rotation, Entry, EntryCharacter, EntryRole
+
+
+class TestRotation(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.testuser = AuthUtils.create_user('aauth_testuser')
+        cls.testcharacter = AuthUtils.add_main_character_2(cls.testuser, 'aauth_testchar', 2116790529)
+
+        cls.rotation: Rotation = Rotation.objects.create(
+            name='test1rot'
+        )
+
+        entry: Entry = Entry.objects.create(
+            rotation=cls.rotation,
+            created_by=cls.testuser,
+            estimated_total=1_000_000_000.0
+        )
+
+        role = EntryRole.objects.create(
+            entry=entry,
+            name='testrole1',
+            value=1
+        )
+
+        share = EntryCharacter.objects.create(
+            entry=entry,
+            user=cls.testuser,
+            user_character=cls.testcharacter,
+            role=role,
+            site_count=1,
+            helped_setup=False
+        )
+
+        entry.update_share_totals()
+
+    def test_summary_open(self):
+        summary = self.rotation.summary
+
+        self.assertEqual(summary.count(), 1)
+
+        row = summary.first()
+
+        self.assertEqual(row.user, self.testuser)
+        self.assertEqual(row.helped_setups, 0)
+        self.assertEqual(row.estimated_total, 1_000_000_000)
+        self.assertEqual(row.actual_total, 0)
+
+    def test_summary_closed(self):
+        self.rotation.actual_total = 900_000_000
+        self.rotation.is_closed = True
+        self.rotation.closed_at = timezone.now()
+        self.rotation.save()
+        for e in self.rotation.entries.all():
+            e.update_share_totals()
+
+        summary = self.rotation.summary
+
+        self.assertEqual(summary.count(), 1)
+
+        row = summary.first()
+
+        self.assertEqual(row.user, self.testuser)
+        self.assertEqual(row.helped_setups, 0)
+        self.assertEqual(row.estimated_total, 1_000_000_000)
+        self.assertEqual(row.actual_total, 900_000_000)
+
+    def test_days_since(self):
+        self.assertEqual(self.rotation.days_since, 0)
+
+    def test_sales_percentage_open(self):
+        self.assertEqual(self.rotation.sales_percentage, 0)
+
+    def test_sales_percentage_closed(self):
+        self.rotation.actual_total = 900_000_000
+        self.rotation.is_closed = True
+        self.rotation.closed_at = timezone.now()
+        self.rotation.save()
+        for e in self.rotation.entries.all():
+            e.update_share_totals()
+
+        self.assertAlmostEqual(self.rotation.sales_percentage, 0.9)
+
+    def test_estimated_total(self):
+        self.assertAlmostEqual(self.rotation.estimated_total, 1_000_000_000.0)
+
+    def test_str(self):
+        self.assertEqual(str(self.rotation), f'{self.rotation.pk} {self.rotation.name}')
