f20434a59c6f8fdca3345575dd65d22b6feb7b8d
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-28 15:46:39 +0200

    added create rotation tests

--- allianceauth_pve/tests/test_views.py
+++ allianceauth_pve/tests/test_views.py
@@ -89,3 +89,52 @@ class TestDeleteEntryView(TestCase):
 
         self.assertRedirects(response, reverse('allianceauth_pve:rotation_view', args=[self.rotation.pk]))
         self.assertEqual(self.rotation.entries.count(), 1)
+
+
+class TestCreateRotationView(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.testuser = AuthUtils.create_user('aauth_testuser')
+        cls.testcharacter = AuthUtils.add_main_character_2(cls.testuser, 'aauth_testchar', 2116790529)
+
+        cls.testuser = AuthUtils.add_permissions_to_user_by_name(['allianceauth_pve.access_pve', 'allianceauth_pve.manage_rotations'], cls.testuser)
+
+    def test_get(self):
+        self.client.force_login(self.testuser)
+
+        response = self.client.get(reverse('allianceauth_pve:new_rotation'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'allianceauth_pve/rotation_create.html')
+
+    def test_post_success(self):
+        self.client.force_login(self.testuser)
+
+        form_data = {
+            'name': 'testrot1',
+            'priority': '1',
+            'tax_rate': '1.0',
+        }
+
+        response = self.client.post(reverse('allianceauth_pve:new_rotation'), form_data)
+
+        self.assertEqual(Rotation.objects.count(), 1)
+
+        rotation = Rotation.objects.get(name='testrot1')
+
+        self.assertRedirects(response, reverse('allianceauth_pve:rotation_view', args=[rotation.pk]))
+
+    def test_post_invalid(self):
+        self.client.force_login(self.testuser)
+
+        form_data = {
+            'name': 'testrot1',
+            'priority': '1',
+            'tax_rate': '1.0',
+            'min_people_share_setup': '-5'
+        }
+
+        response = self.client.post(reverse('allianceauth_pve:new_rotation'), form_data)
+
+        self.assertTemplateUsed(response, 'allianceauth_pve/rotation_create.html')
