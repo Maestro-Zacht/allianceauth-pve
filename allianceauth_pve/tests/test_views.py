e728b3a6327ee8db37aa0fe9f6c9443af3a1c939
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-28 15:00:08 +0200

    added test for dashboard

--- allianceauth_pve/tests/test_views.py
+++ allianceauth_pve/tests/test_views.py
@@ -18,3 +18,20 @@ class TestIndex(TestCase):
 
         response = self.client.get(reverse('allianceauth_pve:index'))
         self.assertRedirects(response, reverse('allianceauth_pve:dashboard'))
+
+
+class TestDashboard(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.testuser = AuthUtils.create_user('aauth_testuser')
+        cls.testcharacter = AuthUtils.add_main_character_2(cls.testuser, 'aauth_testchar', 2116790529)
+
+        cls.testuser = AuthUtils.add_permissions_to_user_by_name(['allianceauth_pve.access_pve'], cls.testuser)
+
+    def test_dashboard(self):
+        self.client.force_login(self.testuser)
+
+        response = self.client.get(reverse('allianceauth_pve:dashboard'))
+
+        self.assertTemplateUsed(response, 'allianceauth_pve/ratting-dashboard.html')
