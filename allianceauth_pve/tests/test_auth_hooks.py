0ce726acbafbc6e254e46d45c00b1e1dad5a50b8
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-08-28 12:55:50 +0200

    added auth hooks tests

---
+++ allianceauth_pve/tests/test_auth_hooks.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from allianceauth.tests.auth_utils import AuthUtils
+
+
+class TestHooks(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.testuser = AuthUtils.create_user('aauth_testuser')
+        cls.testcharacter = AuthUtils.add_main_character_2(cls.testuser, 'aauth_testchar', 2116790529)
+
+    def test_render_hook_success(self):
+        self.testuser = AuthUtils.add_permissions_to_user_by_name(['allianceauth_pve.access_pve'], self.testuser)
+
+        self.client.force_login(self.testuser)
+
+        response = self.client.get(reverse('allianceauth_pve:index'))
+        self.assertRedirects(response, reverse('allianceauth_pve:dashboard'))
+
+    def test_render_hook_fail(self):
+        self.client.force_login(self.testuser)
+
+        response = self.client.get(reverse('allianceauth_pve:index'))
+        self.assertEqual(response.status_code, 403)
