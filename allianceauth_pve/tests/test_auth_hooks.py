6dba05c6dfbdf4ea3df25aa135557c0715c3a44d
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-09-15 17:41:52 +0200

    fixed assertions

--- allianceauth_pve/tests/test_auth_hooks.py
+++ allianceauth_pve/tests/test_auth_hooks.py
@@ -12,13 +12,21 @@ class TestHooks(TestCase):
         cls.testuser = AuthUtils.create_user('aauth_testuser')
         cls.testcharacter = AuthUtils.add_main_character_2(cls.testuser, 'aauth_testchar', 2116790529)
 
+        cls.html_menu = """
+            <li>
+                <a class="active" href="/pve/">
+                    <i class="fas fa-wallet"></i> PvE Tool
+                </a>
+            </li>
+        """
+
     def test_render_hook_success(self):
         self.testuser = AuthUtils.add_permissions_to_user_by_name(['allianceauth_pve.access_pve'], self.testuser)
 
         self.client.force_login(self.testuser)
 
         response = self.client.get(reverse('allianceauth_pve:index'))
-        self.assertRedirects(response, reverse('allianceauth_pve:dashboard'))
+        self.assertContains(response, self.html_menu, html=True)
 
     def test_render_hook_fail(self):
         self.testuser = AuthUtils.add_permissions_to_user_by_name(['allianceauth_pve.manage_rotations'], self.testuser)
@@ -26,4 +34,4 @@ class TestHooks(TestCase):
         self.client.force_login(self.testuser)
 
         response = self.client.get(reverse('allianceauth_pve:new_rotation'))
-        self.assertRedirects(response, reverse(settings.LOGIN_URL) + f"?next={reverse('allianceauth_pve:new_rotation')}")
+        self.assertNotContains(response, self.html_menu, html=True)
