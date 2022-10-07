3478824a3badfa2e511f9076617ea1e346f3b0cf
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2022-10-07 23:50:05 +0200

    added js for button

--- allianceauth_pve/static/allianceauth_pve/js/copy_text.js
+++ allianceauth_pve/static/allianceauth_pve/js/copy_text.js
@@ -1,14 +1,21 @@
 function copyText(event) {
     const text = event.currentTarget.textContent;
     navigator.clipboard.writeText(text);
-    window.alert("Copied: " + text);
+    spop({
+        template: "Copied: " + text,
+        style: "info",
+        autoclose: 5000,
+        position: 'bottom-right',
+    });
 }
 
-function rowCopied(event) {
+function toggleRow(event) {
     if (event.target.classList.contains("copy-text")) {
         event.currentTarget.classList.add('copied');
+    } else if (event.target.classList.contains("undo-copy")) {
+        event.currentTarget.classList.remove('copied');
     }
 }
 
 $(".copy-text").on("click", copyText);
-$(".copy-row").on("click", rowCopied);
+$(".copy-row").on("click", toggleRow);
