417aa6f96013a1392ea147398307fc8e69f1a31f
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2024-01-03 17:53:49 +0100

    move string to setu up class

--- allianceauth_pve/tests/test_auth_hooks.py
+++ allianceauth_pve/tests/test_auth_hooks.py
@@ -7,20 +7,23 @@ from allianceauth.tests.auth_utils import AuthUtils
 class TestHooks(TestCase):
 
     @classmethod
-    def setUpTestData(cls):
-        cls.testuser = AuthUtils.create_user('aauth_testuser')
-        cls.testcharacter = AuthUtils.add_main_character_2(cls.testuser, 'aauth_testchar', 2116790529)
-
+    def setUpClass(cls):
+        super().setUpClass()
         cls.html_menu = f"""
             <a class="nav-link flex-fill align-self-center" href="/pve/">
                 PvE Tool
             </a>
         """
 
-    def test_render_hook_success(self):
-        self.testuser = AuthUtils.add_permission_to_user_by_name('allianceauth_pve.access_pve', self.testuser)
+    @classmethod
+    def setUpTestData(cls):
+        cls.testuser = AuthUtils.create_user('aauth_testuser')
+        cls.testcharacter = AuthUtils.add_main_character_2(cls.testuser, 'aauth_testchar', 2116790529)
 
-        self.client.force_login(self.testuser)
+    def test_render_hook_success(self):
+        user = AuthUtils.add_permission_to_user_by_name('allianceauth_pve.access_pve', self.testuser)
+
+        self.client.force_login(user)
 
         response = self.client.get(reverse('allianceauth_pve:dashboard'))
         self.assertContains(response, self.html_menu, html=True)
