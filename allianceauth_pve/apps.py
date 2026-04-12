af08b9ded18be961f0611d06ad7373cc416694a6
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2026-04-12 21:52:01 +0200

    add version in admin

--- allianceauth_pve/apps.py
+++ allianceauth_pve/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from . import __version__
 
 
 class AllianceauthPveConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'allianceauth_pve'
+    verbose_name = f"AllianceAuth PvE {__version__}"
